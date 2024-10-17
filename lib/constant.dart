f45517e21baa15581201be38fed0a1cdf2cb7332
Rahulthakur8521 <145336545+Rahulthakur8521@users.noreply.github.com>
2024-10-17 12:32:17 +0530

    added

---
+++ lib/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:357676492800:web:9eb59809ee7ab0a59990e5";
+  static String apiKey = "AIzaSyAgTA4VT7MKN5v7TzM1fWyUGk86vODWdmE";
+  static String messagingSenderId = "357676492800";
+  static String projectId = "flutter-c8e50";
+  final primaryColor = const Color(0xFFee7b64);
+}
